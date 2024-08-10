8c83eb411f83cb50e22da3394c5ac37572d94ce1
Amir <amirligvany@gmail.com>
2024-08-10 16:04:09 +0330

    recorder file update

---
+++ handMotionOps/data.py
@@ -0,0 +1,6 @@
+from models import Command, Gesture
+
+
+def save(gesture: Gesture, command: Command):
+    # TODO save as json
+    pass
--- handMotionOps/recorder.py
+++ handMotionOps/recorder.py
@@ -1,7 +1,39 @@
-#we decet and save hand gestures
-#TODO reaad commands
-#TODO get gesture
-#TODO save as json file
+# we decet and save hand gestures
+# TODO reaad commands
+# TODO get gesture
+# TODO save as json file
+
+import cv2
+import mediapipe as mp
+from models import Gesture, Command
+from data import save
 
 
+command = input("enter command")
+print('show the gesture you want to save to this command')
 
+
+vid = cv2.VideoCapture(0)
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+while True:
+    success, img = vid.read()
+    h, w, c = img.shape
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        hand = results.multi_hand_landmarks[0]
+        G = Gesture(hand)
+        c = Command(command)
+        save(G, c)
+        print("save succesful")
+        cv2.destroyAllWindows()
+        exit()
+
+    cv2.imshow("your video", img)
+    if cv2.waitkey(1) & 0xFF == ord('q'):
+        break
