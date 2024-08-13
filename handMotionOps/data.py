5883990df9b8f65431755c40708534e6bb43f2e4
Amir <amirligvany@gmail.com>
2024-08-13 22:05:25 +0330

    update

--- handMotionOps/data.py
+++ handMotionOps/data.py
@@ -1,44 +1,41 @@
 from models import Command, Gesture, get_gesture
 from json import dumps, loads
 
-
-
+# we define a function to save the data recieved from user to our json file
 
 
 def save(gesture: Gesture, command: Command):
-    f = open("/Users/hadi/pythonProjects/opencv-vision-lab/handMotionOps/data/data.json")
-
+    # open json file thta we save our data in it
+    # TODO ask Hadi
+    f = open("./data/data.json")
+    # deserialize a JSON string into a Python object and read it
     l = loads(f.read())
-
+    # check if gesture is already added
     for i in l:
         g = get_gesture(i["gesture"])
         c = Command(i["command"])
 
         if g == gesture and c == command:
             return
-        
-        if g == gesture and c!= command:
+
+        if g == gesture and c != command:
             l.pop({
                 "gesture": gesture.to_string(),
                 "command": command.text
             })
-
+    # Add the new command and gesture to the file
     l.append({
         "gesture": gesture.to_string(),
         "command": command.text
     })
 
-    f = open("/Users/hadi/pythonProjects/opencv-vision-lab/handMotionOps/data/data.json", "w+")
-    
+    f = open(
+        "/Users/hadi/pythonProjects/opencv-vision-lab/handMotionOps/data/data.json", "w+")
+    # serialize the Python object to a JSON formatted string
     f.write(dumps(l))
 
     f.close()
 
-
-
-
-    
-    
 
 save(
     Gesture(None),
