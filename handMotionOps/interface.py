3571e4efce6eff28fc275f43453551b014d0adc1
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-15 14:52:05 +0330

    added interface

--- handMotionOps/interface.py
+++ handMotionOps/interface.py
@@ -1,19 +1,64 @@
 import cv2
 import mediapipe as mp
+from models import Gesture, Command
+from time import time
 
-# initialazation
-cap = cv2.VideoCapture(0)
-mphands = mp.solutions.hands
-hands = mphands.Hands()
-mpDraw = mp.solutions.drawing_utils
+duration = 3
+r = 150
 
 
-while True:
-    success, img = cap.read()
-    h, w, c = img.shape
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+def destroy_windows():
+    cv2.destroyAllWindows()
 
-    cv2.imshow('ur video', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
+class Interface:
+
+    def __init__(self, event):
+        self.start_time = None
+        self.gesture = None
+        self.event = event
+        self.vid = cv2.VideoCapture('/Users/hadi/pythonProjects/opencv/session1/sample_video.mp4')
+
+        self.hands = mp.solutions.hands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
+
+        self.gesture: Gesture
+        self.start_time: float
+
+    def loop(self):
+        while True:
+            success, img = self.vid.read()
+            h, w, c = img.shape
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = self.hands.process(imgRGB)
+
+            if results.multi_hand_landmarks:
+                for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
+                    if results.multi_handedness[idx].classification[0].index == 0:
+                        g = Gesture(hand_landmarks)
+                        self.mpDraw.draw_landmarks(
+                            img, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
+                        if not self.gesture or g.fingers != self.gesture.fingers:
+                            start_time = time()
+                            self.gesture = g
+                        cx5, cy5 = int(
+                            hand_landmarks.landmark[5].x * w), int(hand_landmarks.landmark[5].y * h)
+                        cv2.circle(img, (cx5, cy5), r, (255, 255, 255), 20)
+                        cv2.ellipse(img, (cx5, cy5), (r, r), 0, -90, -
+                        90 + (time() - start_time) * (360 / duration), (0, 80, 0), 20)
+                        print(start_time)
+            else:
+                self.start_time = None
+                self.gesture = None
+
+            if start_time and start_time + duration <= time():
+                self.event(self.gesture)
+
+            cv2.imshow("your video", img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    def start(self):
+        # t = Thread(target=self.loop)
+        # t.start()
+        self.loop()
--- handMotionOps/recorder.py
+++ handMotionOps/recorder.py
@@ -3,68 +3,25 @@
 # TODO get gesture
 # TODO save as json file
 
-
-import cv2
-import mediapipe as mp
 from models import Gesture, Command
 from data import save
-from time import time
-from math import sqrt
+from interface import Interface, destroy_windows
 
 print("Welcome to HandMotionOps.")
 
-command = Command(input("enter commnad :"))
+command = Command(input("enter command :"))
 
 print("Show gesture to camera with your left hand, hold it for 3 seconds to save ...")
 
-vid = cv2.VideoCapture(1)
 
-hands = mp.solutions.hands.Hands()
-mpDraw = mp.solutions.drawing_utils
-
-duration = 3
-
-gesture: Gesture = None
-start_time: float = None
-r = 150
-
-
-def end():
+def end(g: Gesture):
     save(g, command)
     print("saved successfully")
-    cv2.destroyAllWindows()
+    destroy_windows()
     exit()
 
 
+i = Interface(end)
+
 if __name__ == "__main__":
-
-    while True:
-        success, img = vid.read()
-        h, w, c = img.shape
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = hands.process(imgRGB)
-
-        if results.multi_hand_landmarks:
-            for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
-                if results.multi_handedness[idx].classification[0].index == 0:
-                    g = Gesture(hand_landmarks)
-                    mpDraw.draw_landmarks(
-                        img, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
-                    if not gesture or g.fingers != gesture.fingers:
-                        start_time = time()
-                        gesture = g
-                    cx5, cy5 = int(
-                        hand_landmarks.landmark[5].x * w), int(hand_landmarks.landmark[5].y * h)
-                    cv2.circle(img, (cx5, cy5), r, (255, 255, 255), 20)
-                    cv2.ellipse(img, (cx5, cy5), (r, r), 0, -90, -
-                                90 + (time() - start_time) * (360/duration), (0, 80, 0), 20)
-        else:
-            start_time = None
-            gesture = None
-
-        if start_time and start_time + duration <= time():
-            end()
-
-        cv2.imshow("your video", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    i.start()
