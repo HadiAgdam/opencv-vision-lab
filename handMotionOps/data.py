a527a8934f3eca3e38c46f5ad2afc7d1a42ceb29
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-22 13:04:45 +0330

    added get command by gesture

--- handMotionOps/data.py
+++ handMotionOps/data.py
@@ -1,20 +1,17 @@
 from models import Command, Gesture, get_gesture
 from json import dumps, loads
 
-# we define a function to save the data recieved from user to our json file
+
+# we define a function to save the data received from user to our json file
 
 
 def save(gesture: Gesture, command: Command):
-    # ./ is incorrenct. If you want to get a file from upper directory, you should use ../
-
     # open json file that we save our data in it
-
     with open("data/data.json", 'r') as f:
         data_list = loads(f.read())
 
-    data_list = [item for item in data_list]
-    for idx, ges in enumerate(data_list):
-        if ges["gesture"] == gesture.to_string():
+    for idx, i in enumerate(data_list):
+        if i["gesture"] == gesture.to_string():
             data_list.pop(idx)
 
     data_list.append({
@@ -36,4 +33,11 @@ def read():
     return saved_gestures
 
 
+def get_command_by_gesture(g: Gesture) -> Command:
+    with open("data/data.json", 'r') as f:
+        data_list = loads(f.read())
+
+        for i in data_list:
+            if i["gesture"] == g.to_string():
+                return Command(i["command"])
 
