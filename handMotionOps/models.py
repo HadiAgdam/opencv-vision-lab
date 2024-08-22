8f6198cab9003cbf90468968ecfda30c389f0d4b
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-08-22 13:06:42 +0330

    updated gesture to be able to parse from string

--- handMotionOps/models.py
+++ handMotionOps/models.py
@@ -1,13 +1,7 @@
 # get gesture and convert it to string
 class Gesture:
-    def __init__(self, hand) -> None:
-        self.fingers = [False]*5
-
-        self.fingers[0] = hand.landmark[4].x > hand.landmark[2].x
-        self.fingers[1] = hand.landmark[8].y < hand.landmark[6].y
-        self.fingers[2] = hand.landmark[12].y < hand.landmark[10].y
-        self.fingers[3] = hand.landmark[16].y < hand.landmark[14].y
-        self.fingers[4] = hand.landmark[20].y < hand.landmark[18].y
+    def __init__(self, fingers) -> None:
+        self.fingers = fingers
 
     def to_string(self) -> str:
         j = ""
@@ -23,9 +17,27 @@ class Command:
     def __init__(self, text: str) -> None:
         self.text = text
 
+    def execute(self):
+        # TODO execute maybe using system.os
+        pass
+
+
+def get_gesture_from_hand(hand) -> Gesture:
+    fingers = [hand.landmark[4].x > hand.landmark[2].x,
+               hand.landmark[8].y < hand.landmark[6].y,
+               hand.landmark[12].y < hand.landmark[10].y,
+               hand.landmark[16].y < hand.landmark[14].y,
+               hand.landmark[20].y < hand.landmark[18].y]
+    return Gesture(fingers)
+
 
 def get_gesture(gesture: str) -> Gesture:
-    l2 = []
-    for k in gesture:
-        l2.append(k == '1')
-        # TODO return gesture
+    fingers = []
+
+    for i in gesture:
+        if i == "0":
+            fingers.append(False)
+        elif i == "1":
+            fingers.append(True)
+
+    return Gesture(fingers)
